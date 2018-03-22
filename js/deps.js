0e565a0aca8952e421a258904c3c0ebcce871eac
Khmel Mykyta <hmelenok@gmail.com>
2018-03-23 01:27:42 +0200

    Groomed a bit slides

--- js/deps.js
+++ js/deps.js
@@ -7,5 +7,16 @@ Reveal.initialize({
     { src: 'plugin/markdown/markdown.js' },
     { src: 'plugin/notes/notes.js', async: true },
     { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
-  ]
+  ],
+  // Parallax background image
+  parallaxBackgroundImage: 'assets/10.jpg', // e.g. "https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg"
+
+  // Parallax background size
+  parallaxBackgroundSize: '2470px 4109px', // CSS syntax, e.g. "2100px 900px" - currently only pixels are supported (don't use % or auto)
+
+  // Number of pixels to move the parallax background per slide
+  // - Calculated automatically unless specified
+  // - Set to 0 to disable movement along an axis
+  parallaxBackgroundHorizontal: 2470 / 7,
+  parallaxBackgroundVertical: 4109 / 4
 });
