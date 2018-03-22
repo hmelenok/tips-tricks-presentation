3caafa9864b71fc2ea29824c977f03dbb23287f6
Khmel Mykyta <hmelenok@gmail.com>
2018-03-23 01:57:12 +0200

    fix images

--- js/deps.js
+++ js/deps.js
@@ -9,7 +9,7 @@ Reveal.initialize({
     { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
   ],
   // Parallax background image
-  parallaxBackgroundImage: 'assets/10.jpg', // e.g. "https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg"
+  parallaxBackgroundImage: 'images/10.jpg', // e.g. "https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg"
 
   // Parallax background size
   parallaxBackgroundSize: '2470px 4109px', // CSS syntax, e.g. "2100px 900px" - currently only pixels are supported (don't use % or auto)
