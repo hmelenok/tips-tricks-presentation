f8a558042de202018a3529c81910a95b36fa053a
Khmel Mykyta <hmelenok@gmail.com>
2018-03-22 19:05:17 +0200

    Initial version

---
+++ js/deps.js
@@ -0,0 +1,11 @@
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+Reveal.initialize({
+  dependencies: [
+    { src: 'plugin/markdown/marked.js' },
+    { src: 'plugin/markdown/markdown.js' },
+    { src: 'plugin/notes/notes.js', async: true },
+    { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+  ]
+});
